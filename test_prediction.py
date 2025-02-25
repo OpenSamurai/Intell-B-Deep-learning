e7bad80eaf9df093c886ec5b6641df7f342e8531
apocalypse9949 <125962989+apocalypse9949@users.noreply.github.com>
2025-02-25 19:39:06 +0530

    Update test_prediction.py

--- test_prediction.py
+++ test_prediction.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     model = load_model('Data/Model/model.json', 'Data/Model/weights.weights.h5')
     
     # Create a dummy input
-    test_input = np.random.rand(1, 150, 150, 3)  # Example shape for an image input
+    test_input = np.random.rand(1, 150, 150, 3)  # Example shape for an image input😒
     print("Testing prediction...")
     Y = model.predict(test_input)
     print("Prediction output:", Y) 
